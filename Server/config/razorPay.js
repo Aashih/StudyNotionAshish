b0c86070c785e16efbfe694b34e6824a621134ac
Ashish <ashishpanchtilak@gmail.com>
2024-09-20 13:53:58 +0530

    fist vala

---
+++ Server/config/razorPay.js
@@ -0,0 +1,8 @@
+const Razorpay = require("razorpay");
+
+//geeting the instance of razor pay 
+
+exports.instance = new Razorpay({
+    key_id:process.env.RAZORPAY_KEY,
+    key_secret:process.env.RAZORPAY_SECRET,
+})
