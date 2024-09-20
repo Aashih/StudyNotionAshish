7340ae35158204bc9f83906af3a4a4055870402d
Ashish <ashishpanchtilak@gmail.com>
2024-09-20 16:11:13 +0530

    email

--- Server/controllers/ResetPassword.js
+++ Server/controllers/ResetPassword.js
@@ -34,7 +34,7 @@ exports.resetPasswordToken = async(req,res)=>{
         // console.log("DETAILS",updatedDetails);
         //create url
         // const url = `http://localhost:3000/update-password/${token}`;
-         const url = `https://studynotion-edtech-project.vercel.app/update-password/${token}`
+         const url = `https://study-notion-ashish-db88.vercel.app/update-password/${token}`
 
         //send mail containing the url 
 
